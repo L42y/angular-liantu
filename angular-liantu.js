0a8059f67141767c3fbdd5c2a91d48316e54daaf
L42y <423300@gmail.com>
2014-08-20 07:17:56 +0800

    refactor: watch all attributes to reflect changes

--- angular-liantu.js
+++ angular-liantu.js
@@ -1,28 +1,33 @@
 angular.module('l42y.liantu', []).directive('liantu', function (
   $window
 ) {
+  var name = 'liantu';
   var pixelRatio = $window.getDevicePixelRatio().toFixed();
 
   return {
     restrict: 'A',
     link: function ($scope, $element, $attrs) {
-      var name = 'liantu';
-      var size = $attrs.width;
-      var sizeStr = size ? '&w=' + size * pixelRatio : '';
-      var text = encodeURIComponent($attrs.liantu);
-      var params = '';
-      angular.forEach($attrs, function (value, key) {
-        if (key.substring(0, name.length) === name &&
-            key.length > name.length) {
-          var param = key.substring(name.length).toLowerCase();
-          params = params +
-            '&' + param +
-            '=' + $attrs[key];
-        }
+      $scope.$watchCollection(function () {
+        return $attrs;
+      }, function (attrs) {
+        var size = $attrs.width;
+        var sizeStr = size ? '&w=' + size * pixelRatio : '';
+        var text = encodeURIComponent($attrs.liantu);
+        var params = '';
+        angular.forEach($attrs, function (value, key) {
+          if (key.substring(0, name.length) === name &&
+              key.length > name.length) {
+            var param = key.substring(name.length).toLowerCase();
+            params = params +
+              '&' + param +
+              '=' + $attrs[key];
+          }
+        });
+
+        var src = '//qr.liantu.com/api.php?text=' + text +
+              sizeStr + params;
+        $attrs.$set('src', src);
       });
-      var src = '//qr.liantu.com/api.php?text=' + text +
-            sizeStr + params;
-      $attrs.$set('src', src);
     }
   };
 });
